b8bd0ecf05fe8e474a16360472493454f65017b8
Sorin Baciu <sorin.baciu@uzinex.ro>
2026-04-09 21:42:02 +0300

    Hero video: scale 1.15 zoom-in to crop YouTube branding bleed

--- app/noutati/HeroFeatured.tsx
+++ app/noutati/HeroFeatured.tsx
@@ -92,12 +92,16 @@ export function HeroFeatured({
           {videoId ? (
             <>
               {/* YouTube iframe sized + scaled to crop black bars */}
-              <div className="absolute inset-0 pointer-events-none">
+              <div className="absolute inset-0 pointer-events-none overflow-hidden">
                 <iframe
                   src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}&controls=0&modestbranding=1&playsinline=1&rel=0&showinfo=0&iv_load_policy=3&disablekb=1&fs=0`}
                   title={article.title}
                   allow="autoplay; encrypted-media; picture-in-picture"
-                  className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-w-[177.78vh] min-h-[56.25vw] w-[200%] h-[200%] border-0"
+                  className="absolute top-1/2 left-1/2 min-w-[177.78vh] min-h-[56.25vw] w-[200%] h-[200%] border-0"
+                  style={{
+                    transform: "translate(-50%, -50%) scale(1.15)",
+                    transformOrigin: "center center",
+                  }}
                   frameBorder={0}
                 />
               </div>
